51caae85a85679da826738203c213f4f6bcbf751
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 16:56:07 +0100

    Update legacy16.sql

--- [SQL]/legacy16.sql
+++ [SQL]/legacy16.sql
@@ -1,1 +1,59 @@
+--
+-- Table structure for table `whitelist`
+--
+
+CREATE TABLE `whitelist` (
+	`identifier` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addon_account`
+--
+ALTER TABLE `addon_account`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `addon_account_data`
+--
+ALTER TABLE `addon_account_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_addon_account_data_account_name_owner` (`account_name`,`owner`),
+  ADD KEY `index_addon_account_data_account_name` (`account_name`);
+
+--
+-- Indexes for table `addon_inventory`
+--
+ALTER TABLE `addon_inventory`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `addon_inventory_items`
+--
+ALTER TABLE `addon_inventory_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `index_addon_inventory_items_inventory_name_name` (`inventory_name`,`name`),
+  ADD KEY `index_addon_inventory_items_inventory_name_name_owner` (`inventory_name`,`name`,`owner`),
+  ADD KEY `index_addon_inventory_inventory_name` (`inventory_name`);
+
+--
+-- Indexes for table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cardealer_vehicles`
+--
+ALTER TABLE `cardealer_vehicles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `datastore`
+--
+ALTER TABLE `datastore`
+  ADD PRIMARY KEY (`name`);
 
