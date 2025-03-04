14e124e5330f58caef8e6e4b4a1660098bcb0837
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 16:58:53 +0100

    Update legacy18.sql

--- [SQL]/legacy18.sql
+++ [SQL]/legacy18.sql
@@ -1,1 +1,57 @@
+--
+-- Indexes for table `society_moneywash`
+--
+ALTER TABLE `society_moneywash`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`identifier`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `user_licenses`
+--
+ALTER TABLE `user_licenses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vehicle_categories`
+--
+ALTER TABLE `vehicle_categories`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `whitelist`
+--
+ALTER TABLE `whitelist`
+  ADD PRIMARY KEY (`identifier`);
+
+--
+-- AUTO_INCREMENT for table `addon_account_data`
+--
+ALTER TABLE `addon_account_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `addon_inventory_items`
+--
+ALTER TABLE `addon_inventory_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cardealer_vehicles`
+--
+ALTER TABLE `cardealer_vehicles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
 
