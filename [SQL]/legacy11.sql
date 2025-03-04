2ae8678c98290d63367f219cd5ba72952d64a12f
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 14:45:48 +0100

    Update legacy11.sql

--- [SQL]/legacy11.sql
+++ [SQL]/legacy11.sql
@@ -1,1 +1,24 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `licenses`
+--
+
+CREATE TABLE `licenses` (
+  `type` varchar(60) NOT NULL,
+  `label` varchar(60) NOT NULL
+) ENGINE=InnoDB;
+
+--
+-- Dumping data for table `licenses`
+--
+
+INSERT INTO `licenses` (`type`, `label`) VALUES
+('dmv', 'Driving Permit'),
+('drive', 'Drivers License'),
+('drive_bike', 'Motorcycle License'),
+('drive_truck', 'Commercial Drivers License'),
+('weed_processing', 'Weed Processing License'),
+('boat', 'Boat License');
+
 
