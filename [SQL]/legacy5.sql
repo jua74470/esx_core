f03b00ca4eea00c27c895b68d03e192165882f1f
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 06:02:40 +0100

    Update legacy5.sql

--- [SQL]/legacy5.sql
+++ [SQL]/legacy5.sql
@@ -1,1 +1,603 @@
+--
+-- Table structure for table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `name` varchar(60) NOT NULL,
+  `model` varchar(60) NOT NULL,
+  `price` int(11) NOT NULL,
+  `category` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB;
+
+--
+-- Dumping data for table `vehicles`
+--
+
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES
+('Adder', 'adder', 900000, 'super'),
+('Akuma', 'AKUMA', 7500, 'motorcycles'),
+('Alpha', 'alpha', 60000, 'sports'),
+('Ardent', 'ardent', 1150000, 'sportsclassics'),
+('Asea', 'asea', 5500, 'sedans'),
+('Autarch', 'autarch', 1955000, 'super'),
+('Avarus', 'avarus', 18000, 'motorcycles'),
+('Bagger', 'bagger', 13500, 'motorcycles'),
+('Baller', 'baller2', 40000, 'suvs'),
+('Baller Sport', 'baller3', 60000, 'suvs'),
+('Banshee', 'banshee', 70000, 'sports'),
+('Banshee 900R', 'banshee2', 255000, 'super'),
+('Bati 801', 'bati', 12000, 'motorcycles'),
+('Bati 801RR', 'bati2', 19000, 'motorcycles'),
+('Bestia GTS', 'bestiagts', 55000, 'sports'),
+('BF400', 'bf400', 6500, 'motorcycles'),
+('Bf Injection', 'bfinjection', 16000, 'offroad'),
+('Bifta', 'bifta', 12000, 'offroad'),
+('Bison', 'bison', 45000, 'vans'),
+('Blade', 'blade', 15000, 'muscle'),
+('Blazer', 'blazer', 6500, 'offroad'),
+('Blazer Sport', 'blazer4', 8500, 'offroad'),
+('blazer5', 'blazer5', 1755600, 'offroad'),
+('Blista', 'blista', 8000, 'compacts'),
+('BMX (velo)', 'bmx', 160, 'motorcycles'),
+('Bobcat XL', 'bobcatxl', 32000, 'vans'),
+('Brawler', 'brawler', 45000, 'offroad'),
+('Brioso R/A', 'brioso', 18000, 'compacts'),
+('Btype', 'btype', 62000, 'sportsclassics'),
+('Btype Hotroad', 'btype2', 155000, 'sportsclassics'),
+('Btype Luxe', 'btype3', 85000, 'sportsclassics'),
+('Buccaneer', 'buccaneer', 18000, 'muscle'),
+('Buccaneer Rider', 'buccaneer2', 24000, 'muscle'),
+('Buffalo', 'buffalo', 12000, 'sports'),
+('Buffalo S', 'buffalo2', 20000, 'sports'),
+('Bullet', 'bullet', 90000, 'super'),
+('Burrito', 'burrito3', 19000, 'vans'),
+('Camper', 'camper', 42000, 'vans'),
+('Carbonizzare', 'carbonizzare', 75000, 'sports'),
+('Carbon RS', 'carbonrs', 18000, 'motorcycles'),
+('Casco', 'casco', 30000, 'sportsclassics'),
+('Cavalcade', 'cavalcade2', 55000, 'suvs'),
+('Cheetah', 'cheetah', 375000, 'super'),
+('Chimera', 'chimera', 38000, 'motorcycles'),
+('Chino', 'chino', 15000, 'muscle'),
+('Chino Luxe', 'chino2', 19000, 'muscle'),
+('Cliffhanger', 'cliffhanger', 9500, 'motorcycles'),
+('Cognoscenti Cabrio', 'cogcabrio', 55000, 'coupes'),
+('Cognoscenti', 'cognoscenti', 55000, 'sedans'),
+('Comet', 'comet2', 65000, 'sports'),
+('Comet 5', 'comet5', 1145000, 'sports'),
+('Contender', 'contender', 70000, 'suvs'),
+('Coquette', 'coquette', 65000, 'sports'),
+('Coquette Classic', 'coquette2', 40000, 'sportsclassics'),
+('Coquette BlackFin', 'coquette3', 55000, 'muscle'),
+('Cruiser (velo)', 'cruiser', 510, 'motorcycles'),
+('Cyclone', 'cyclone', 1890000, 'super'),
+('Daemon', 'daemon', 11500, 'motorcycles'),
+('Daemon High', 'daemon2', 13500, 'motorcycles'),
+('Defiler', 'defiler', 9800, 'motorcycles'),
+('Deluxo', 'deluxo', 4721500, 'sportsclassics'),
+('Dominator', 'dominator', 35000, 'muscle'),
+('Double T', 'double', 28000, 'motorcycles'),
+('Dubsta', 'dubsta', 45000, 'suvs'),
+('Dubsta Luxuary', 'dubsta2', 60000, 'suvs'),
+('Bubsta 6x6', 'dubsta3', 120000, 'offroad'),
+('Dukes', 'dukes', 28000, 'muscle'),
+('Dune Buggy', 'dune', 8000, 'offroad'),
+('Elegy', 'elegy2', 38500, 'sports'),
+('Emperor', 'emperor', 8500, 'sedans'),
+('Enduro', 'enduro', 5500, 'motorcycles'),
+('Entity XF', 'entityxf', 425000, 'super'),
+('Esskey', 'esskey', 4200, 'motorcycles'),
+('Exemplar', 'exemplar', 32000, 'coupes'),
+('F620', 'f620', 40000, 'coupes'),
+('Faction', 'faction', 20000, 'muscle'),
+('Faction Rider', 'faction2', 30000, 'muscle'),
+('Faction XL', 'faction3', 40000, 'muscle'),
+('Faggio', 'faggio', 1900, 'motorcycles'),
+('Vespa', 'faggio2', 2800, 'motorcycles'),
+('Felon', 'felon', 42000, 'coupes'),
+('Felon GT', 'felon2', 55000, 'coupes'),
+('Feltzer', 'feltzer2', 55000, 'sports'),
+('Stirling GT', 'feltzer3', 65000, 'sportsclassics'),
+('Fixter (velo)', 'fixter', 225, 'motorcycles'),
+('FMJ', 'fmj', 185000, 'super'),
+('Fhantom', 'fq2', 17000, 'suvs'),
+('Fugitive', 'fugitive', 12000, 'sedans'),
+('Furore GT', 'furoregt', 45000, 'sports'),
+('Fusilade', 'fusilade', 40000, 'sports'),
+('Gargoyle', 'gargoyle', 16500, 'motorcycles'),
+('Gauntlet', 'gauntlet', 30000, 'muscle'),
+('Gang Burrito', 'gburrito', 45000, 'vans'),
+('Burrito', 'gburrito2', 29000, 'vans'),
+('Glendale', 'glendale', 6500, 'sedans'),
+('Grabger', 'granger', 50000, 'suvs'),
+('Gresley', 'gresley', 47500, 'suvs'),
+('GT 500', 'gt500', 785000, 'sportsclassics'),
+('Guardian', 'guardian', 45000, 'offroad'),
+('Hakuchou', 'hakuchou', 31000, 'motorcycles'),
+('Hakuchou Sport', 'hakuchou2', 55000, 'motorcycles'),
+('Hermes', 'hermes', 535000, 'muscle'),
+('Hexer', 'hexer', 12000, 'motorcycles'),
+('Hotknife', 'hotknife', 125000, 'muscle'),
+('Huntley S', 'huntley', 40000, 'suvs'),
+('Hustler', 'hustler', 625000, 'muscle'),
+('Infernus', 'infernus', 180000, 'super'),
+('Innovation', 'innovation', 23500, 'motorcycles'),
+('Intruder', 'intruder', 7500, 'sedans'),
+('Issi', 'issi2', 10000, 'compacts'),
+('Jackal', 'jackal', 38000, 'coupes'),
+('Jester', 'jester', 65000, 'sports'),
+('Jester(Racecar)', 'jester2', 135000, 'sports'),
+('Journey', 'journey', 6500, 'vans'),
+('Kamacho', 'kamacho', 345000, 'offroad'),
+('Khamelion', 'khamelion', 38000, 'sports'),
+('Kuruma', 'kuruma', 30000, 'sports'),
+('Landstalker', 'landstalker', 35000, 'suvs'),
+('RE-7B', 'le7b', 325000, 'super'),
+('Lynx', 'lynx', 40000, 'sports'),
+('Mamba', 'mamba', 70000, 'sports'),
+('Manana', 'manana', 12800, 'sportsclassics'),
+('Manchez', 'manchez', 5300, 'motorcycles'),
+('Massacro', 'massacro', 65000, 'sports'),
+('Massacro(Racecar)', 'massacro2', 130000, 'sports'),
+('Mesa', 'mesa', 16000, 'suvs'),
+('Mesa Trail', 'mesa3', 40000, 'suvs'),
+('Minivan', 'minivan', 13000, 'vans'),
+('Monroe', 'monroe', 55000, 'sportsclassics'),
+('The Liberator', 'monster', 210000, 'offroad'),
+('Moonbeam', 'moonbeam', 18000, 'vans'),
+('Moonbeam Rider', 'moonbeam2', 35000, 'vans'),
+('Nemesis', 'nemesis', 5800, 'motorcycles'),
+('Neon', 'neon', 1500000, 'sports'),
+('Nightblade', 'nightblade', 35000, 'motorcycles'),
+('Nightshade', 'nightshade', 65000, 'muscle'),
+('9F', 'ninef', 65000, 'sports'),
+('9F Cabrio', 'ninef2', 80000, 'sports'),
+('Omnis', 'omnis', 35000, 'sports'),
+('Oppressor', 'oppressor', 3524500, 'super'),
+('Oracle XS', 'oracle2', 35000, 'coupes'),
+('Osiris', 'osiris', 160000, 'super'),
+('Panto', 'panto', 10000, 'compacts'),
+('Paradise', 'paradise', 19000, 'vans'),
+('Pariah', 'pariah', 1420000, 'sports'),
+('Patriot', 'patriot', 55000, 'suvs'),
+('PCJ-600', 'pcj', 6200, 'motorcycles'),
+('Penumbra', 'penumbra', 28000, 'sports'),
+('Pfister', 'pfister811', 85000, 'super'),
+('Phoenix', 'phoenix', 12500, 'muscle'),
+('Picador', 'picador', 18000, 'muscle'),
+('Pigalle', 'pigalle', 20000, 'sportsclassics'),
+('Prairie', 'prairie', 12000, 'compacts'),
+('Premier', 'premier', 8000, 'sedans'),
+('Primo Custom', 'primo2', 14000, 'sedans'),
+('X80 Proto', 'prototipo', 2500000, 'super'),
+('Radius', 'radi', 29000, 'suvs'),
+('raiden', 'raiden', 1375000, 'sports'),
+('Rapid GT', 'rapidgt', 35000, 'sports'),
+('Rapid GT Convertible', 'rapidgt2', 45000, 'sports'),
+('Rapid GT3', 'rapidgt3', 885000, 'sportsclassics'),
+('Reaper', 'reaper', 150000, 'super'),
+('Rebel', 'rebel2', 35000, 'offroad'),
+('Regina', 'regina', 5000, 'sedans'),
+('Retinue', 'retinue', 615000, 'sportsclassics'),
+('Revolter', 'revolter', 1610000, 'sports'),
+('riata', 'riata', 380000, 'offroad'),
+('Rocoto', 'rocoto', 45000, 'suvs'),
+('Ruffian', 'ruffian', 6800, 'motorcycles'),
+('Ruiner 2', 'ruiner2', 5745600, 'muscle'),
+('Rumpo', 'rumpo', 15000, 'vans'),
+('Rumpo Trail', 'rumpo3', 19500, 'vans'),
+('Sabre Turbo', 'sabregt', 20000, 'muscle'),
+('Sabre GT', 'sabregt2', 25000, 'muscle'),
+('Sanchez', 'sanchez', 5300, 'motorcycles'),
+('Sanchez Sport', 'sanchez2', 5300, 'motorcycles'),
+('Sanctus', 'sanctus', 25000, 'motorcycles'),
+('Sandking', 'sandking', 55000, 'offroad'),
+('Savestra', 'savestra', 990000, 'sportsclassics'),
+('SC 1', 'sc1', 1603000, 'super'),
+('Schafter', 'schafter2', 25000, 'sedans'),
+('Schafter V12', 'schafter3', 50000, 'sports'),
+('Scorcher (velo)', 'scorcher', 280, 'motorcycles'),
+('Seminole', 'seminole', 25000, 'suvs'),
+('Sentinel', 'sentinel', 32000, 'coupes'),
+('Sentinel XS', 'sentinel2', 40000, 'coupes'),
+('Sentinel3', 'sentinel3', 650000, 'sports'),
+('Seven 70', 'seven70', 39500, 'sports'),
+('ETR1', 'sheava', 220000, 'super'),
+('Shotaro Concept', 'shotaro', 320000, 'motorcycles'),
+('Slam Van', 'slamvan3', 11500, 'muscle'),
+('Sovereign', 'sovereign', 22000, 'motorcycles'),
+('Stinger', 'stinger', 80000, 'sportsclassics'),
+('Stinger GT', 'stingergt', 75000, 'sportsclassics'),
+('Streiter', 'streiter', 500000, 'sports'),
+('Stretch', 'stretch', 90000, 'sedans'),
+('Stromberg', 'stromberg', 3185350, 'sports'),
+('Sultan', 'sultan', 15000, 'sports'),
+('Sultan RS', 'sultanrs', 65000, 'super'),
+('Super Diamond', 'superd', 130000, 'sedans'),
+('Surano', 'surano', 50000, 'sports'),
+('Surfer', 'surfer', 12000, 'vans'),
+('T20', 't20', 300000, 'super'),
+('Tailgater', 'tailgater', 30000, 'sedans'),
+('Tampa', 'tampa', 16000, 'muscle'),
+('Drift Tampa', 'tampa2', 80000, 'sports'),
+('Thrust', 'thrust', 24000, 'motorcycles'),
+('Tri bike (velo)', 'tribike3', 520, 'motorcycles'),
+('Trophy Truck', 'trophytruck', 60000, 'offroad'),
+('Trophy Truck Limited', 'trophytruck2', 80000, 'offroad'),
+('Tropos', 'tropos', 40000, 'sports'),
+('Turismo R', 'turismor', 350000, 'super'),
+('Tyrus', 'tyrus', 600000, 'super'),
+('Vacca', 'vacca', 120000, 'super'),
+('Vader', 'vader', 7200, 'motorcycles'),
+('Verlierer', 'verlierer2', 70000, 'sports'),
+('Vigero', 'vigero', 12500, 'muscle'),
+('Virgo', 'virgo', 14000, 'muscle'),
+('Viseris', 'viseris', 875000, 'sportsclassics'),
+('Visione', 'visione', 2250000, 'super'),
+('Voltic', 'voltic', 90000, 'super'),
+('Voltic 2', 'voltic2', 3830400, 'super'),
+('Voodoo', 'voodoo', 7200, 'muscle'),
+('Vortex', 'vortex', 9800, 'motorcycles'),
+('Warrener', 'warrener', 4000, 'sedans'),
+('Washington', 'washington', 9000, 'sedans'),
+('Windsor', 'windsor', 95000, 'coupes'),
+('Windsor Drop', 'windsor2', 125000, 'coupes'),
+('Woflsbane', 'wolfsbane', 9000, 'motorcycles'),
+('XLS', 'xls', 32000, 'suvs'),
+('Yosemite', 'yosemite', 485000, 'muscle'),
+('Youga', 'youga', 10800, 'vans'),
+('Youga Luxuary', 'youga2', 14500, 'vans'),
+('Z190', 'z190', 900000, 'sportsclassics'),
+('Zentorno', 'zentorno', 1500000, 'super'),
+('Zion', 'zion', 36000, 'coupes'),
+('Zion Cabrio', 'zion2', 45000, 'coupes'),
+('Zombie', 'zombiea', 9500, 'motorcycles'),
+('Zombie Luxuary', 'zombieb', 12000, 'motorcycles'),
+('Z-Type', 'ztype', 220000, 'sportsclassics');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle_categories`
+--
+
+CREATE TABLE `vehicle_categories` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(60) NOT NULL
+) ENGINE=InnoDB;
+
+--
+-- Dumping data for table `vehicle_categories`
+--
+
+INSERT INTO `vehicle_categories` (`name`, `label`) VALUES
+('compacts', 'Compacts'),
+('coupes', 'Coupés'),
+('motorcycles', 'Motos'),
+('muscle', 'Muscle'),
+('offroad', 'Off Road'),
+('sedans', 'Sedans'),
+('sports', 'Sports'),
+('sportsclassics', 'Sports Classics'),
+('super', 'Super'),
+('suvs', 'SUVs'),
+('vans', 'Vans');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicle_sold`
+--
+
+CREATE TABLE `vehicle_sold` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `plate` varchar(50) NOT NULL,
+  `soldby` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `whitelist`
+--
+
+CREATE TABLE `whitelist` (
+	`identifier` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addon_account`
+--
+ALTER TABLE `addon_account`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `addon_account_data`
+--
+ALTER TABLE `addon_account_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_addon_account_data_account_name_owner` (`account_name`,`owner`),
+  ADD KEY `index_addon_account_data_account_name` (`account_name`);
+
+--
+-- Indexes for table `addon_inventory`
+--
+ALTER TABLE `addon_inventory`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `addon_inventory_items`
+--
+ALTER TABLE `addon_inventory_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `index_addon_inventory_items_inventory_name_name` (`inventory_name`,`name`),
+  ADD KEY `index_addon_inventory_items_inventory_name_name_owner` (`inventory_name`,`name`,`owner`),
+  ADD KEY `index_addon_inventory_inventory_name` (`inventory_name`);
+
+--
+-- Indexes for table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cardealer_vehicles`
+--
+ALTER TABLE `cardealer_vehicles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `datastore`
+--
+ALTER TABLE `datastore`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `datastore_data`
+--
+ALTER TABLE `datastore_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_datastore_data_name_owner` (`name`,`owner`),
+  ADD KEY `index_datastore_data_name` (`name`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `job_grades`
+--
+ALTER TABLE `job_grades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `licenses`
+--
+ALTER TABLE `licenses`
+  ADD PRIMARY KEY (`type`);
+
+--
+--
+-- Indexes for table `owned_vehicles`
+--
+ALTER TABLE `owned_vehicles`
+  ADD PRIMARY KEY (`plate`);
+
+--
+--
+-- Indexes for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`model`);
+
+--
+-- Indexes for table `rented_vehicles`
+--
+ALTER TABLE `rented_vehicles`
+  ADD PRIMARY KEY (`plate`);
+
+--
+-- Indexes for table `society_moneywash`
+--
+ALTER TABLE `society_moneywash`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`identifier`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `user_licenses`
+--
+ALTER TABLE `user_licenses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vehicle_categories`
+--
+ALTER TABLE `vehicle_categories`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `whitelist`
+--
+ALTER TABLE `whitelist`
+  ADD PRIMARY KEY (`identifier`);
+
+--
+-- AUTO_INCREMENT for table `addon_account_data`
+--
+ALTER TABLE `addon_account_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `addon_inventory_items`
+--
+ALTER TABLE `addon_inventory_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cardealer_vehicles`
+--
+ALTER TABLE `cardealer_vehicles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `datastore_data`
+--
+ALTER TABLE `datastore_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `job_grades`
+--
+ALTER TABLE `job_grades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+
+-- AUTO_INCREMENT for table `society_moneywash`
+--
+ALTER TABLE `society_moneywash`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_licenses`
+--
+ALTER TABLE `user_licenses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  
+--
+-- Fine Types
+--
+	CREATE TABLE `fine_types` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`label` varchar(255) DEFAULT NULL,
+	`amount` int DEFAULT NULL,
+	`category` int DEFAULT NULL,
+
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+INSERT INTO `fine_types` (label, amount, category) VALUES
+	('Misuse of a horn', 30, 0),
+	('Illegally Crossing a continuous Line', 40, 0),
+	('Driving on the wrong side of the road', 250, 0),
+	('Illegal U-Turn', 250, 0),
+	('Illegally Driving Off-road', 170, 0),
+	('Refusing a Lawful Command', 30, 0),
+	('Illegally Stopping a Vehicle', 150, 0),
+	('Illegal Parking', 70, 0),
+	('Failing to Yield to the right', 70, 0),
+	('Failure to comply with Vehicle Information', 90, 0),
+	('Failing to stop at a Stop Sign ', 105, 0),
+	('Failing to stop at a Red Light', 130, 0),
+	('Illegal Passing', 100, 0),
+	('Driving an illegal Vehicle', 100, 0),
+	('Driving without a License', 1500, 0),
+	('Hit and Run', 800, 0),
+	('Exceeding Speeds Over < 5 mph', 90, 0),
+	('Exceeding Speeds Over 5-15 mph', 120, 0),
+	('Exceeding Speeds Over 15-30 mph', 180, 0),
+	('Exceeding Speeds Over > 30 mph', 300, 0),
+	('Impeding traffic flow', 110, 1),
+	('Public Intoxication', 90, 1),
+	('Disorderly conduct', 90, 1),
+	('Obstruction of Justice', 130, 1),
+	('Insults towards Civilans', 75, 1),
+	('Disrespecting of an LEO', 110, 1),
+	('Verbal Threat towards a Civilan', 90, 1),
+	('Verbal Threat towards an LEO', 150, 1),
+	('Providing False Information', 250, 1),
+	('Attempt of Corruption', 1500, 1),
+	('Brandishing a weapon in city Limits', 120, 2),
+	('Brandishing a Lethal Weapon in city Limits', 300, 2),
+	('No Firearms License', 600, 2),
+	('Possession of an Illegal Weapon', 700, 2),
+	('Possession of Burglary Tools', 300, 2),
+	('Grand Theft Auto', 1800, 2),
+	('Intent to Sell/Distrube of an illegal Substance', 1500, 2),
+	('Frabrication of an Illegal Substance', 1500, 2),
+	('Possession of an Illegal Substance ', 650, 2),
+	('Kidnapping of a Civilan', 1500, 2),
+	('Kidnapping of an LEO', 2000, 2),
+	('Robbery', 650, 2),
+	('Armed Robbery of a Store', 650, 2),
+	('Armed Robbery of a Bank', 1500, 2),
+	('Assault on a Civilian', 2000, 3),
+	('Assault of an LEO', 2500, 3),
+	('Attempt of Murder of a Civilian', 3000, 3),
+	('Attempt of Murder of an LEO', 5000, 3),
+	('Murder of a Civilian', 10000, 3),
+	('Murder of an LEO', 30000, 3),
+	('Involuntary manslaughter', 1800, 3),
+	('Fraud', 2000, 2);
+
+
+--
+-- ESX Bankerjob
+--
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_banker','Bank',1),
+	('bank_savings','Savings account',0)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('banker','Banker')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('banker',0,'advisor','Consultant',10,'{}','{}'),
+	('banker',1,'banker','Banker',20,'{}','{}'),
+	('banker',2,'business_banker',"Investment banker",30,'{}','{}'),
+	('banker',3,'trader','Broker',40,'{}','{}'),
+	('banker',4,'boss','Boss',0,'{}','{}')
+;
+
+--
+-- ESX Banking
+--
+
+CREATE TABLE IF NOT EXISTS `banking` (
+  `identifier` varchar(46) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `amount` int(64) DEFAULT NULL,
+  `time` bigint(20) DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `balance` int(11) DEFAULT 0,
+  `label` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `users` ADD COLUMN `pincode` INT NULL;
+
 
