8d4893255213ced65fe818a3fb415e059b9b5094
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 14:20:46 +0100

    Update legacy7.sql

--- [SQL]/legacy7.sql
+++ [SQL]/legacy7.sql
@@ -65,27 +65,3 @@ INSERT INTO `fine_types` (label, amount, category) VALUES
 	('Fraud', 2000, 2);
 
 
---
--- ESX Bankerjob
---
-
-INSERT INTO `addon_account` (name, label, shared) VALUES
-	('society_banker','Bank',1),
-	('bank_savings','Savings account',0)
-;
-
-INSERT INTO `jobs` (name, label) VALUES
-	('banker','Banker')
-;
-
-INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('banker',0,'advisor','Consultant',10,'{}','{}'),
-	('banker',1,'banker','Banker',20,'{}','{}'),
-	('banker',2,'business_banker',"Investment banker",30,'{}','{}'),
-	('banker',3,'trader','Broker',40,'{}','{}'),
-	('banker',4,'boss','Boss',0,'{}','{}')
-;
-
-
-
-
