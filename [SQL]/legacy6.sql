a134a820deca9c19486099b16b7cf08fd79b25df
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 23:28:33 +0100

    Update legacy6.sql

--- [SQL]/legacy6.sql
+++ [SQL]/legacy6.sql
@@ -15,16 +15,16 @@ CREATE TABLE `datastore` (
 --
 
 INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
-('property', 'Property', 0),
-('society_ambulance', 'EMS', 1),
-('society_mechanic', 'Mechanic', 1),
-('society_police', 'Police', 1),
+('property', 'Propriete', 0),
+('society_ambulance', 'Ambulancier', 1),
+('society_mechanic', 'Mechanicien', 1),
+('society_police', 'Policier', 1),
 ('society_taxi', 'Taxi', 1);
 
 
 INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
-    ('user_ears', 'Ears', 0),
-    ('user_glasses', 'Glasses', 0),
-    ('user_helmet', 'Helmet', 0),
-    ('user_mask', 'Mask', 0);
+    ('user_ears', 'Oreilles', 0),
+    ('user_glasses', 'Lunettes', 0),
+    ('user_helmet', 'Casque', 0),
+    ('user_mask', 'Masque', 0);
     
