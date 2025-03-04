31850ceb1e123e4c4961faab938d2f7b94f659ca
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 16:59:50 +0100

    Update legacy20.sql

--- [SQL]/legacy20.sql
+++ [SQL]/legacy20.sql
@@ -1,1 +1,38 @@
+--
+-- ESX Bankerjob
+--
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_banker','Bank',1),
+	('bank_savings','Savings account',0)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('banker','Banker')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('banker',0,'advisor','Consultant',10,'{}','{}'),
+	('banker',1,'banker','Banker',20,'{}','{}'),
+	('banker',2,'business_banker',"Investment banker",30,'{}','{}'),
+	('banker',3,'trader','Broker',40,'{}','{}'),
+	('banker',4,'boss','Boss',0,'{}','{}')
+;
+
+--
+-- ESX Banking
+--
+
+CREATE TABLE IF NOT EXISTS `banking` (
+  `identifier` varchar(46) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `amount` int(64) DEFAULT NULL,
+  `time` bigint(20) DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `balance` int(11) DEFAULT 0,
+  `label` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `users` ADD COLUMN `pincode` INT NULL;
 
