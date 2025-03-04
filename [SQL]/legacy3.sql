dd9844267d0a8bb2885210c82709dac1bf36ca44
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 14:32:25 +0100

    Update legacy3.sql

--- [SQL]/legacy3.sql
+++ [SQL]/legacy3.sql
@@ -1,78 +1,23 @@
-
--- --------------------------------------------------------
-
 --
--- Table structure for table `datastore_data`
+-- Table structure for table `addon_inventory`
 --
 
-CREATE TABLE `datastore_data` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `addon_inventory` (
   `name` varchar(60) NOT NULL,
-  `owner` varchar(60) DEFAULT NULL,
-  `data` longtext DEFAULT NULL
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL
 ) ENGINE=InnoDB;
 
 --
--- Dumping data for table `datastore_data`
+-- Dumping data for table `addon_inventory`
 --
 
-INSERT INTO `datastore_data` (`id`, `name`, `owner`, `data`) VALUES
-(1, 'society_police', NULL, '{}'),
-(2, 'society_ambulance', NULL, '{}'),
-(3, 'society_mechanic', NULL, '{}'),
-(4, 'society_taxi', NULL, '{}'),
-(5, 'property', NULL, '{}');
+INSERT INTO `addon_inventory` (`name`, `label`, `shared`) VALUES
+('society_ambulance', 'EMS', 1),
+('society_cardealer', 'Cardealer', 1),
+('society_mechanic', 'Mechanic', 1),
+('society_police', 'Police', 1),
+('society_taxi', 'Taxi', 1);
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `items`
---
-
-CREATE TABLE `items` (
-  `name` varchar(50) NOT NULL,
-  `label` varchar(50) NOT NULL,
-  `weight` int(11) NOT NULL DEFAULT 1,
-  `rare` tinyint(4) NOT NULL DEFAULT 0,
-  `can_remove` tinyint(4) NOT NULL DEFAULT 1
-) ENGINE=InnoDB;
-
---
--- Dumping data for table `items`
---
-
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-('alive_chicken', 'Living chicken', 1, 0, 1),
-('bandage', 'Bandage', 2, 0, 1),
-('blowpipe', 'Blowtorch', 2, 0, 1),
-('bread', 'Bread', 1, 0, 1),
-('cannabis', 'Cannabis', 3, 0, 1),
-('carokit', 'Body Kit', 3, 0, 1),
-('carotool', 'Tools', 2, 0, 1),
-('clothe', 'Cloth', 1, 0, 1),
-('copper', 'Copper', 1, 0, 1),
-('cutted_wood', 'Cut wood', 1, 0, 1),
-('diamond', 'Diamond', 1, 0, 1),
-('essence', 'Gas', 1, 0, 1),
-('fabric', 'Fabric', 1, 0, 1),
-('fish', 'Fish', 1, 0, 1),
-('radio', 'Radio', 1, 0, 1),
-('fixkit', 'Repair Kit', 3, 0, 1),
-('fixtool', 'Repair Tools', 2, 0, 1),
-('gazbottle', 'Gas Bottle', 2, 0, 1),
-('gold', 'Gold', 1, 0, 1),
-('iron', 'Iron', 1, 0, 1),
-('marijuana', 'Marijuana', 2, 0, 1),
-('medikit', 'Medikit', 2, 0, 1),
-('packaged_chicken', 'Chicken fillet', 1, 0, 1),
-('packaged_plank', 'Packaged wood', 1, 0, 1),
-('petrol', 'Oil', 1, 0, 1),
-('petrol_raffin', 'Processed oil', 1, 0, 1),
-('phone', 'Phone', 1, 0, 1),
-('slaughtered_chicken', 'Slaughtered chicken', 1, 0, 1),
-('stone', 'Stone', 1, 0, 1),
-('washed_stone', 'Washed stone', 1, 0, 1),
-('water', 'Water', 1, 0, 1),
-('wood', 'Wood', 1, 0, 1),
-('wool', 'Wool', 1, 0, 1);
 
