aaff3d88f19727775f22ef1aa09b353c60d00664
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 14:47:42 +0100

    Update legacy13.sql

--- [SQL]/legacy13.sql
+++ [SQL]/legacy13.sql
@@ -1,1 +1,44 @@
+--
+-- Table structure for table `society_moneywash`
+--
+
+CREATE TABLE `society_moneywash` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(60) NOT NULL,
+  `society` varchar(60) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `identifier` varchar(60) NOT NULL,
+  `accounts` longtext DEFAULT NULL,
+  `group` varchar(50) DEFAULT 'user',
+  `inventory` longtext DEFAULT NULL,
+  `job` varchar(20) DEFAULT 'unemployed',
+  `job_grade` int(11) DEFAULT 0,
+  `loadout` longtext DEFAULT NULL,
+  `metadata` LONGTEXT NULL DEFAULT NULL,
+  `position` longtext NULL DEFAULT NULL,
+  `firstname` varchar(16) DEFAULT NULL,
+  `lastname` varchar(16) DEFAULT NULL,
+  `dateofbirth` varchar(10) DEFAULT NULL,
+  `sex` varchar(1) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  `skin` longtext DEFAULT NULL,
+  `status` longtext DEFAULT NULL,
+  `is_dead` tinyint(1) DEFAULT 0,
+  `id` int(11) NOT NULL,
+  `disabled` TINYINT(1) NULL DEFAULT '0',
+  `last_property` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_seen` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `phone_number` VARCHAR(20) DEFAULT NULL
+) ENGINE=InnoDB;
+
 
