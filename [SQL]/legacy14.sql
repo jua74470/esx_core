1a2ae8cb9d95a38dd36dfb0ec5a662425f2faa1b
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 14:48:12 +0100

    Update legacy14.sql

--- [SQL]/legacy14.sql
+++ [SQL]/legacy14.sql
@@ -1,1 +1,27 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_licenses`
+--
+
+CREATE TABLE `user_licenses` (
+  `id` int(11) NOT NULL,
+  `type` varchar(60) NOT NULL,
+  `owner` varchar(60) NOT NULL
+) ENGINE=InnoDB;
+
+
+
+
+--
+-- Table structure for table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `name` varchar(60) NOT NULL,
+  `model` varchar(60) NOT NULL,
+  `price` int(11) NOT NULL,
+  `category` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB;
+
 
