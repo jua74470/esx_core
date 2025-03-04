fd76e0f8c1d795429c68c067c393b3ae920bfd1a
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 14:46:38 +0100

    Update legacy12.sql

--- [SQL]/legacy12.sql
+++ [SQL]/legacy12.sql
@@ -1,1 +1,31 @@
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `owned_vehicles`
+--
+
+CREATE TABLE `owned_vehicles` (
+  `owner` varchar(60) DEFAULT NULL,
+  `plate` varchar(12) NOT NULL,
+  `vehicle` longtext DEFAULT NULL,
+  `type` varchar(20) NOT NULL DEFAULT 'car',
+  `job` varchar(20) DEFAULT NULL,
+  `stored` tinyint(4) NOT NULL DEFAULT 0,
+  `parking` VARCHAR(60) DEFAULT NULL,
+  `pound` VARCHAR(60) DEFAULT NULL
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rented_vehicles`
+--
+
+CREATE TABLE `rented_vehicles` (
+  `vehicle` varchar(60) NOT NULL,
+  `plate` varchar(12) NOT NULL,
+  `player_name` varchar(255) NOT NULL,
+  `base_price` int(11) NOT NULL,
+  `rent_price` int(11) NOT NULL,
+  `owner` varchar(22) NOT NULL
+) ENGINE=InnoDB;
