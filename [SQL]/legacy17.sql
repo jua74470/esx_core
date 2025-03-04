8360ce6961067bee59684daf653813da6640cb90
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 16:57:22 +0100

    Update legacy17.sql

--- [SQL]/legacy17.sql
+++ [SQL]/legacy17.sql
@@ -1,1 +1,53 @@
+--
+-- Indexes for table `datastore_data`
+--
+ALTER TABLE `datastore_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_datastore_data_name_owner` (`name`,`owner`),
+  ADD KEY `index_datastore_data_name` (`name`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `job_grades`
+--
+ALTER TABLE `job_grades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `licenses`
+--
+ALTER TABLE `licenses`
+  ADD PRIMARY KEY (`type`);
+
+--
+--
+-- Indexes for table `owned_vehicles`
+--
+ALTER TABLE `owned_vehicles`
+  ADD PRIMARY KEY (`plate`);
+
+--
+--
+-- Indexes for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`model`);
+
+--
+-- Indexes for table `rented_vehicles`
+--
+ALTER TABLE `rented_vehicles`
+  ADD PRIMARY KEY (`plate`);
+
 
