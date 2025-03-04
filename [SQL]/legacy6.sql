bec447456e7821940c898193f29bd946b265dc07
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 14:36:37 +0100

    Update legacy6.sql

--- [SQL]/legacy6.sql
+++ [SQL]/legacy6.sql
@@ -1,197 +1,30 @@
---
--- Table structure for table `whitelist`
---
-
-CREATE TABLE `whitelist` (
-	`identifier` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Table structure for table `datastore`
 --
 
---
--- Indexes for table `addon_account`
---
-ALTER TABLE `addon_account`
-  ADD PRIMARY KEY (`name`);
+CREATE TABLE `datastore` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL
+) ENGINE=InnoDB;
 
 --
--- Indexes for table `addon_account_data`
+-- Dumping data for table `datastore`
 --
-ALTER TABLE `addon_account_data`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `index_addon_account_data_account_name_owner` (`account_name`,`owner`),
-  ADD KEY `index_addon_account_data_account_name` (`account_name`);
 
---
--- Indexes for table `addon_inventory`
---
-ALTER TABLE `addon_inventory`
-  ADD PRIMARY KEY (`name`);
-
---
--- Indexes for table `addon_inventory_items`
---
-ALTER TABLE `addon_inventory_items`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `index_addon_inventory_items_inventory_name_name` (`inventory_name`,`name`),
-  ADD KEY `index_addon_inventory_items_inventory_name_name_owner` (`inventory_name`,`name`,`owner`),
-  ADD KEY `index_addon_inventory_inventory_name` (`inventory_name`);
-
---
--- Indexes for table `billing`
---
-ALTER TABLE `billing`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `cardealer_vehicles`
---
-ALTER TABLE `cardealer_vehicles`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `datastore`
---
-ALTER TABLE `datastore`
-  ADD PRIMARY KEY (`name`);
-
---
--- Indexes for table `datastore_data`
---
-ALTER TABLE `datastore_data`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `index_datastore_data_name_owner` (`name`,`owner`),
-  ADD KEY `index_datastore_data_name` (`name`);
-
---
--- Indexes for table `items`
---
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`name`);
-
---
--- Indexes for table `jobs`
---
-ALTER TABLE `jobs`
-  ADD PRIMARY KEY (`name`);
-
---
--- Indexes for table `job_grades`
---
-ALTER TABLE `job_grades`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `licenses`
---
-ALTER TABLE `licenses`
-  ADD PRIMARY KEY (`type`);
-
---
---
--- Indexes for table `owned_vehicles`
---
-ALTER TABLE `owned_vehicles`
-  ADD PRIMARY KEY (`plate`);
-
---
---
--- Indexes for table `vehicles`
---
-ALTER TABLE `vehicles`
-  ADD PRIMARY KEY (`model`);
-
---
--- Indexes for table `rented_vehicles`
---
-ALTER TABLE `rented_vehicles`
-  ADD PRIMARY KEY (`plate`);
-
---
--- Indexes for table `society_moneywash`
---
-ALTER TABLE `society_moneywash`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`identifier`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- Indexes for table `user_licenses`
---
-ALTER TABLE `user_licenses`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `vehicle_categories`
---
-ALTER TABLE `vehicle_categories`
-  ADD PRIMARY KEY (`name`);
-
---
--- Indexes for table `whitelist`
---
-ALTER TABLE `whitelist`
-  ADD PRIMARY KEY (`identifier`);
-
---
--- AUTO_INCREMENT for table `addon_account_data`
---
-ALTER TABLE `addon_account_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `addon_inventory_items`
---
-ALTER TABLE `addon_inventory_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `billing`
---
-ALTER TABLE `billing`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `cardealer_vehicles`
---
-ALTER TABLE `cardealer_vehicles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `datastore_data`
---
-ALTER TABLE `datastore_data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `job_grades`
---
-ALTER TABLE `job_grades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
+('property', 'Property', 0),
+('society_ambulance', 'EMS', 1),
+('society_mechanic', 'Mechanic', 1),
+('society_police', 'Police', 1),
+('society_taxi', 'Taxi', 1);
 
 
--- AUTO_INCREMENT for table `society_moneywash`
---
-ALTER TABLE `society_moneywash`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `user_licenses`
---
-ALTER TABLE `user_licenses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-  
-
+INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
+    ('user_ears', 'Ears', 0),
+    ('user_glasses', 'Glasses', 0),
+    ('user_helmet', 'Helmet', 0),
+    ('user_mask', 'Mask', 0);
+    
